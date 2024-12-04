d5de2bb3c2809fb6aef38688c79905fa13aeaea3
Professor MAD <professormad@proton.me>
2024-12-04 12:18:02 -0500

    All work content added

--- src/components/WorkContent.js
+++ src/components/WorkContent.js
@@ -13,55 +13,83 @@ const workContent = [
     },
 
     {
-        title: "ManaMind: The MTG Stockfish",
-        logo: "/images/mana-mind.png",
-        screenshot: "/images/mana-mind-shot.png",
+        title: "Satanic Agenda: Full-Service Home Page",
+        logo: "/images/satan-todo-favicon.png",
+        screenshot: "/images/satan-shot.png",
         github: "/images/github.png",
         link: "/images/link.png",
         icons: [
-            "/images/c.png",
-            "/images/clang.png"
+            "/images/react.png",
+            "/images/css.png",
+            "/images/js.png",
+            "/images/firebase.png",
         ],
-        description:"This is a custom-built MTG engine with a unique class-less, functional approach to card creation, featuring a gameloop that is operated in the terminal. The card_creation engine and the game_loop engine work together to create a one-of-a-kind card playing experience, focused on game functions and data. A work in progress, the goal will be to someday emulate games at high speeds using all of Magic’s cards, making it the “Stockfish” of Magic the Gathering. It is an open-source project."
+        description: "Satanic Agenda is a calendar app with a robust “todo” system, featuring an “upcoming” and “graveyard” mechanic. There are “scrolls” for larger texts and even a “Satan button,” which can be used to call Satan so he can yell at you to...err...motivate you. The app also comes with Hell Weather, Hell News, Hell Sports, and Hell Stocks, so you can be up to date on what’s happening in Hell. Users can use for free and their session will be saved, but to save forever, they must make an account."
     },
 
     {
-        title: "ManaMind: The MTG Stockfish",
-        logo: "/images/mana-mind.png",
-        screenshot: "/images/mana-mind-shot.png",
+        title: "PROFESSOR MAD: ONLINE COMIC BOOK STORE",
+        logo: "/images/favicon-professor-mad.png",
+        screenshot: "/images/mad-shot.png",
         github: "/images/github.png",
         link: "/images/link.png",
         icons: [
-            "/images/c.png",
-            "/images/clang.png"
+            "/images/html.png",
+            "/images/css.png",
+            "/images/js.png",
+            "/images/firebase.png",
+            "/images/stripe.png"
         ],
-        description:"This is a custom-built MTG engine with a unique class-less, functional approach to card creation, featuring a gameloop that is operated in the terminal. The card_creation engine and the game_loop engine work together to create a one-of-a-kind card playing experience, focused on game functions and data. A work in progress, the goal will be to someday emulate games at high speeds using all of Magic’s cards, making it the “Stockfish” of Magic the Gathering. It is an open-source project."
+        description: "Professormad.com is the homepage for the comedic adult-comic book “The Beautiful Shamaryayamayamays of Shnerfadalailisexinr.” The website comes with an e-commerce interface, browser game page, and the fastest-growing public fart-noise MP3 library—all user-generated. An artistic project of the infamous PROFESSOR MAD, the IP is accompanied by a video series and merch line. Buy your copy today and immediately regret your decision! Or maybe just stop by to play DINO NUKE COCKROACH."
+    },
+
+
+    {
+        title: "D-Comply: Compliance Training Dashboard",
+        logo: "/images/DComply-logo.png",
+        screenshot: "/images/dcomply-shot.png",
+        github: "/images/github.png",
+        link: "/images/link.png",
+        icons: [
+            "/images/react.png",
+            "/images/css.png",
+            "/images/js.png",
+            "/images/node.png",
+            "/images/ex.png",
+            "/images/mongo.png",
+        ],
+        description: "D-comply is a clone application of Binghamton University’s B-comply app. It is a compliance training platform in which users go through training sequences featuring point and click tests, videos, and surveys. Their progress is saved in a database and is updated for their needs. It can be controlled through an administrator’s dashboard. It is a WIP, and is primarily used as a mechanism to experiment with back-end solutions. The website is not hosted but its code can be viewed on Github."
     },
 
     {
-        title: "ManaMind: The MTG Stockfish",
-        logo: "/images/mana-mind.png",
-        screenshot: "/images/mana-mind-shot.png",
+        title: "osr tolkien map: online map store",
+        logo: "/images/osr-favicon.png",
+        screenshot: "/images/osr.png",
         github: "/images/github.png",
         link: "/images/link.png",
         icons: [
-            "/images/c.png",
-            "/images/clang.png"
+            "/images/html.png",
+            "/images/css.png",
+            "/images/js.png",
+            "/images/firebase.png",
+            "/images/stripe.png"
         ],
-        description:"This is a custom-built MTG engine with a unique class-less, functional approach to card creation, featuring a gameloop that is operated in the terminal. The card_creation engine and the game_loop engine work together to create a one-of-a-kind card playing experience, focused on game functions and data. A work in progress, the goal will be to someday emulate games at high speeds using all of Magic’s cards, making it the “Stockfish” of Magic the Gathering. It is an open-source project."
+        description: "The OSR Tolkien Map is an art project from the infamous PROFESSOR MAD, showcasing a map of Onteora Scout Reservation, Theodore Roosevelt Council, Scouts BSA, in the spirit of Tolkien, writer of Lord of the Rings and Creator of Middle Earth. The website is a simple e-commerce platform, that has attracted considerable attention from members of our council, both Scouts and Alumni alike. The transaction backend uses Stripe to process payments. The desktop version of the site features a hover zoom feature, made famous by Amazon listings."
     },
 
     {
-        title: "ManaMind: The MTG Stockfish",
-        logo: "/images/mana-mind.png",
-        screenshot: "/images/mana-mind-shot.png",
+        title: "DREAMCAST CENTRIFUGE: RANDOMIZER",
+        logo: "/images/dreamcast-centrifuge.png",
+        screenshot: "/images/dreamcast-shot.png",
         github: "/images/github.png",
         link: "/images/link.png",
         icons: [
-            "/images/c.png",
-            "/images/clang.png"
+            "/images/html.png",
+            "/images/css.png",
+            "/images/js.png",
+            "/images/firebase.png",
         ],
-        description:"This is a custom-built MTG engine with a unique class-less, functional approach to card creation, featuring a gameloop that is operated in the terminal. The card_creation engine and the game_loop engine work together to create a one-of-a-kind card playing experience, focused on game functions and data. A work in progress, the goal will be to someday emulate games at high speeds using all of Magic’s cards, making it the “Stockfish” of Magic the Gathering. It is an open-source project."
+        description: "Have you ever settled down for some Sega Dreamcast when all of a sudden you are stricken with indecision as to which game you should play? Too many decisions! With Dreamcast Centrifuge, you can select your custom list of games and then RANDOMIZE! The app will make the tough decisions for you. Don’t feel like playing Sonic Adventure 2? No problem! Try some Sega Marine Fishing? What’s that? Don’t like fish? How about some Chu Chu Rocket. The world is yours."
     },
 
 ]

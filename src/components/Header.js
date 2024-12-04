ab9b40f6e71baddd12fab033042b6fb4c52408a5
Professor MAD <professormad@proton.me>
2024-12-03 20:57:01 -0500

    Header layout complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,17 @@ import "./Header.css"
 
 function Header() {
     return(
-        <div className="header-container">Header</div>
+        <div className="header-container">
+            <div className="header-left">
+                DIOGUARDI
+            </div>
+            <div className="header-right">
+                <div className="home-button">HOME</div>
+                <div className="work-button">WORK</div>
+                <div className="about-button">ABOUT</div>
+                <div className="contact-button">CONTACT</div>
+            </div>
+        </div>
     );
 }
 

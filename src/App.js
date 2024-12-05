40e9d707a858a1b8ad8de4f20f5d61c53b7e31c4
Professor MAD <professormad@proton.me>
2024-12-05 08:39:16 -0500

    About started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Work from "./components/Work";
+import About from "./components/About";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Header />
             <Home />
             <Work />
+            <About />
         </div>
     );
 }

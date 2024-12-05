cd9ce785423235c177a3badbf8bb5cc007595416
Professor MAD <professormad@proton.me>
2024-12-05 09:46:05 -0500

    about mobile done!

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,64 @@
+import { useState, useEffect } from "react";
 import "./About.css";
 
 function About() {
+    const [ isMobile, setIsMobile ] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 480px)");
+
+        setIsMobile(mediaQuery.matches);
+
+        const handleResize = () => setIsMobile(mediaQuery.matches);
+        mediaQuery.addEventListener("change", handleResize);
+
+        return () =>  mediaQuery.removeEventListener("change", handleResize);
+    }, []);
+
     return (
         <div className="about-parent">
-            <img className="teaching" src="./images/teaching.jpg" alt="teaching"/>
-            <div className="nouns">
-                <ul className="noun-item">FATHER</ul>
-                <ul className="noun-item">HUSBAND</ul>
-                <ul className="noun-item">PROFESSOR</ul>
-                <ul className="noun-item">TEACHER</ul>
-                <ul className="noun-item">EAGLE SCOUT</ul>
-                <ul className="noun-item">COACH</ul>
-                <ul className="noun-item">MENTOR</ul>
-                <ul className="noun-item">WRITER</ul>
-                <ul className="noun-item">ARTIST</ul>
-                <ul className="noun-item">ATHLETE</ul>
-            </div>
+            {isMobile ? (
+                <div className="teaching-wrapper">
+                    <>
+                
+                <img className="teaching" src="./images/teaching.jpg" alt="teaching" />
+                <div className="nouns">
+                    <ul className="noun-item">FATHER</ul>
+                    <ul className="noun-item">HUSBAND</ul>
+                    <ul className="noun-item">PROFESSOR</ul>
+                    <ul className="noun-item">TEACHER</ul>
+                    <ul className="noun-item">EAGLE SCOUT</ul>
+                    <ul className="noun-item">COACH</ul>
+                    <ul className="noun-item">MENTOR</ul>
+                    <ul className="noun-item">WRITER</ul>
+                    <ul className="noun-item">ARTIST</ul>
+                    <ul className="noun-item">ATHLETE</ul>
+                </div>
+            </>
+                </div>
+                
+            ) : (
+                <>
+                    <img className="teaching" src="./images/teaching.jpg" alt="teaching" />
+                    <div className="nouns">
+                        <ul className="noun-item">FATHER</ul>
+                        <ul className="noun-item">HUSBAND</ul>
+                        <ul className="noun-item">PROFESSOR</ul>
+                        <ul className="noun-item">TEACHER</ul>
+                        <ul className="noun-item">EAGLE SCOUT</ul>
+                        <ul className="noun-item">COACH</ul>
+                        <ul className="noun-item">MENTOR</ul>
+                        <ul className="noun-item">WRITER</ul>
+                        <ul className="noun-item">ARTIST</ul>
+                        <ul className="noun-item">ATHLETE</ul>
+                    </div>
+                </>
+            )}
             <div className="description-parent">
                 <div className="recognition-container">
                     <img className="eagle-icon" src="./images/eagle.png" alt="eagle" />
                     <img className="rec-icon" src="./images/ecc.png" alt="ecc" />
+                    <img className="rec-icon" src="./images/30.png" alt="30" />
                     <img className="rec-icon" src="./images/bb.png" alt="bb" />
                 </div>
                 <div className="description-container">

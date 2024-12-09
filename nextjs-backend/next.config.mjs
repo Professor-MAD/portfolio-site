ca581027a049778e96ac2779a00db3d11a70a6eb
Professor MAD <professormad@proton.me>
2024-12-09 08:51:08 -0500

    origin next fonfig edit

--- nextjs-backend/next.config.mjs
+++ nextjs-backend/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         source: "/api/:path*", // Apply to all API routes
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://portfolio-site-c4rd.vercel.app" }, // Frontend URL
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
         ],

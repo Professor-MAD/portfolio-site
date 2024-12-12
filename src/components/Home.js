dbdf97ef76182797da59371761862c8f379efc96
Professor MAD <professormad@proton.me>
2024-12-12 08:25:00 -0500

    home sizing adjustments, see my projects button itialized to scroll to work section

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,20 @@ import "./Home.css";
 function Home() {
     const [isAvatarHovered, setIsAvatarHovered] = useState(false);
 
+    const scrollToSection = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         <div className="home-container">
             <div className="home-left">
                 <div className="Im">I'm</div>
                 <div className="name">MICHAEL DIOGUARDI</div>
                 <div className="description">A coding jack with a focus on developing front-end designs that are simple and memorable.</div>
-                <div className="projects-button">See My Projects</div>
+                <div className="projects-button" onClick={() => scrollToSection("work")}>See My Projects</div>
             </div>
             <div className="home-right">
                 <img

fb9579b09c0ed3a89ad24307de238d91b1407df9
Professor MAD <professormad@proton.me>
2024-12-12 09:28:14 -0500

    about mobile design done

--- src/components/About.js
+++ src/components/About.js
@@ -18,26 +18,36 @@ function About() {
     return (
         <div className="about-parent">
             {isMobile ? (
-                <div className="teaching-wrapper">
-                    <>
-                
-                <img className="teaching" src="./images/teaching.jpg" alt="teaching" />
-                <div className="nouns">
-                    <ul className="noun-item">FATHER</ul>
-                    <ul className="noun-item">HUSBAND</ul>
-                    <ul className="noun-item">PROFESSOR</ul>
-                    <ul className="noun-item">TEACHER</ul>
-                    <ul className="noun-item">EAGLE SCOUT</ul>
-                    <ul className="noun-item">COACH</ul>
-                    <ul className="noun-item">MENTOR</ul>
-                    <ul className="noun-item">WRITER</ul>
-                    <ul className="noun-item">ARTIST</ul>
-                    <ul className="noun-item">ATHLETE</ul>
-                </div>
-            </>
-                </div>
-                
-            ) : (
+        <div className="teaching-wrapper">
+                     <div className="nouns-mobile">
+                            <ul className="noun-item">FATHER</ul>
+                            <ul className="noun-item">HUSBAND</ul>
+                            <ul className="noun-item">PROFESSOR</ul>
+                            <ul className="noun-item">TEACHER</ul>
+                            <ul className="noun-item">EAGLE SCOUT</ul>
+                        </div>
+                        <div className="nouns-mobile">
+                            <ul className="noun-item">COACH</ul>
+                            <ul className="noun-item">MENTOR</ul>
+                            <ul className="noun-item">WRITER</ul>
+                            <ul className="noun-item">ARTIST</ul>
+                            <ul className="noun-item">ATHLETE</ul>
+                        </div>
+
+                    <img className="teaching" src="./images/teaching.jpg" alt="teaching" />
+            <div className="description-parent-mobile">
+                    <div className="recognition-container-mobile">
+                        <img className="eagle-icon-mobile" src="./images/eagle.png" alt="eagle" />
+                        <img className="rec-icon-mobile" src="./images/ecc.png" alt="ecc" />
+                        <img className="rec-icon-mobile" src="./images/30.png" alt="30" />
+                        <img className="rec-icon-mobile" src="./images/bb.png" alt="bb" />
+                    </div>
+                    <div className="description-container-mobile">
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                    </div>
+            </div>
+        </div>
+) : (
                 <>
                     <img className="teaching" src="./images/teaching.jpg" alt="teaching" />
                     <div className="nouns">

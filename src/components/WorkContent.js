f71ebfcfc7f8dc309d9fb451ab4734a84c18f2a9
Professor MAD <professormad@proton.me>
2025-08-14 13:55:42 -0400

    add icons to totes

--- src/components/WorkContent.js
+++ src/components/WorkContent.js
@@ -13,7 +13,9 @@ const workContent = [
       'images/Postgresql_elephant.svg.png',
       '/images/railway.png',
       '/images/stripe.png',
-      '/images/Amazon-S3-Logo.svg.png',
+      '/images/resend.webp',
+      '/images/twilio.svg',
+      '/images/zendesk.png',
     ],
     description:
       "Totes is a peer-to-peer rental platform that makes it easy to rent everyday items from people near you. Whether you need tools, gear, electronics, or party supplies, Totes helps you find what you need without the cost of buying new. Users can list items, set availability, and manage bookings all in one place. It's convenient, eco-friendly, and community-driven—perfect for saving money, reducing waste, and making the most out of the stuff we already own.",

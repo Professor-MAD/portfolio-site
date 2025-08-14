2537f4136253450079c3a7ab93e3b3560e0deef9
Professor MAD <professormad@proton.me>
2025-08-14 14:13:16 -0400

    add font to rtw

--- src/components/WorkContent.js
+++ src/components/WorkContent.js
@@ -104,7 +104,7 @@ const workContent = [
     ],
     description:
       'Red Trail Wanderers is a comprehensive outdoor education and adventure website operated by the Onteora Alumni Association. It offers high adventure programs to Catskill Fire Towers, a 16-stop History Trail with educational content and patches, trail conservation projects on Long Island, camping at OSR Base Camp with access to 470 miles of Catskill trails, Eagle Scout project showcases, and free trail maps. The site serves as a hub for hiking, advancement, and conservation activities, connecting scouts and outdoor enthusiasts with nature experiences and community service opportunities.',
-    fontFamily: 'Bellfoods',
+    fontFamily: 'Nature Breath',
     useVideo: false,
   },
 

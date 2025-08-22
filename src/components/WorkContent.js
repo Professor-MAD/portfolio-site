be30186c0f42757744cf00b506b3bfcebe53dd6b
Professor MAD <professormad@proton.me>
2025-08-22 08:28:32 -0400

    add firebase logo to totes card

--- src/components/WorkContent.js
+++ src/components/WorkContent.js
@@ -13,6 +13,7 @@ const workContent = [
       'images/Postgresql_elephant.svg.png',
       '/images/railway.png',
       '/images/stripe.png',
+      '/images/firebase.png',
       '/images/Amazon-S3-Logo.svg.png',
       '/images/resend.webp',
       '/images/twilio.svg',
